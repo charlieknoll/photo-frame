0a14d5adf67f41282b0ad399f7c6efdef634581c
Charlie Knoll <charlie.knoll@gmail.com>
2020-12-12 16:48:29 -0700

    pwa

--- images.js
+++
@@ -1,4 +0,0 @@
-window.photoFrame = window.photoFrame || {}
-// window.photoFrame.images = [
-//   'https://lh3.googleusercontent.com/pw/ACtC-3fbh_2gqS73w1A9sYuTi7KruFMj6lKXcCphWs_kHXc66gZMBXs9OpQFjU8yvteMhbJJMjfsTe4Cuh_voibxSZH-DyMyXMeGllou-xb__AioQzX8dx_3IaH0c_gPgDAoYmD1mrRkbAhFhrM-Fo3r-D1h9g=w736-h981-no'
-// ]
